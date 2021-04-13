980310191d345c45289051c9c3de69fd45eafe77
Arthur <arthureuw@gmail.com>
2021-04-13 14:34:10 +0200

    Init NodeJS

---
+++ index.js
@@ -0,0 +1,21 @@
+const express=require('express')
+const cors=require('cors')
+const app=express()
+
+const connectDB=require('./config/connectDB')
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//connectDB (config)
+connectDB()
+
+app.use("/contacts",require('./routes/contact'))
+
+
+
+
+//run server
+const port=process.env.PORT||5000
+app.listen(port,err=>err?console.log(err):console.log(`connected on port ${port}`))
