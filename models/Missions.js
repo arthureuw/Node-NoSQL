93f14b5501f49cd87c1a5443b6e350e22e928cab
Arthur <arthureuw@gmail.com>
2021-04-13 15:15:04 +0200

    Added Schema for misision

--- models/Missions.js
+++ models/Missions.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose')
-const schema   = mongoose.Schema;
+const schema = mongoose.Schema;
 
-const MissionSchema= new schema({
-
+const MissionSchema = new schema({
+    name: {type: String, required: true, trim: true},
+    location: {type: String},
+    duration: {type: Number, min: 0},
+    salary: {type: Number, min: 0},
+    ownerId : [
+        {type: mongoose.Schema.Types.ObjectId,ref:'owner'}
+    ],
+    createdAt: {type: Date, required: true, default: Date.now}
 });
 
-module.exports  = mongoose.model('mission',MissionSchema)
+module.exports = mongoose.model('mission', MissionSchema)
+
+
+// skills []
+// ownerId
+
